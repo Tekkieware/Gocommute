b145cfac9a4f32a10d155c6c09a0fe6205de9839
Tekkieware <tekkieware@gmail.com>
2023-06-05 09:12:07 +0100

    routing set up complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is goccomute's official website</h1>
+    <div>
+      <Router>
+            <Routes>
+              <Route path="/" element={<Home />} exact />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+      </Router>
     </div>
   );
 }
